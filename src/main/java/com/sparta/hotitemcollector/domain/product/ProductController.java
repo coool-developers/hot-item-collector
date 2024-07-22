eeac397755aa838d5e21011d9b26d9318a727453
hongsy521 <seoyeong0521@naver.com>
2024-07-22 11:04:10 +0900

    ✨ feat : 상품 등록 controller

--- src/main/java/com/sparta/hotitemcollector/domain/product/ProductController.java
+++ src/main/java/com/sparta/hotitemcollector/domain/product/ProductController.java
@@ -1,4 +1,27 @@
 package com.sparta.hotitemcollector.domain.product;
 
+import com.sparta.hotitemcollector.domain.security.UserDetailsImpl;
+import com.sparta.hotitemcollector.global.common.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<CommonResponse<ProductResponseDto>> createProduct(@RequestBody ProductRequestDto requestDto,
+        UserDetailsImpl userDetails){
+        ProductResponseDto responseDto = productService.createProduct(requestDto,userDetails.getUser());
+        CommonResponse response = new CommonResponse("상품 등록 성공",201,responseDto);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
 }
--- src/main/java/com/sparta/hotitemcollector/domain/product/ProductService.java
+++ src/main/java/com/sparta/hotitemcollector/domain/product/ProductService.java
@@ -1,4 +1,14 @@
 package com.sparta.hotitemcollector.domain.product;
 
+import com.sparta.hotitemcollector.domain.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductResponseDto createProduct(ProductRequestDto requestDto, User user) {
+        return null;
+    }
 }
