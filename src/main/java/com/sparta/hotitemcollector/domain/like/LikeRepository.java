4474ffa63f3925f6aaef0711ffa73dc8c9ad3e86
WonGi-Kim <kwg970310@gmail.com>
2024-07-22 11:04:13 +0900

    [#1] ♻️ refactor : @Repository 어노테이션 제거

--- src/main/java/com/sparta/hotitemcollector/domain/like/LikeRepository.java
+++ src/main/java/com/sparta/hotitemcollector/domain/like/LikeRepository.java
@@ -2,11 +2,9 @@ package com.sparta.hotitemcollector.domain.like;
 
 import com.sparta.hotitemcollector.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface LikeRepository extends JpaRepository<Likes, Long> {
     Optional<Likes> findByProductIdAndUser(Long productId, User user);
 }
