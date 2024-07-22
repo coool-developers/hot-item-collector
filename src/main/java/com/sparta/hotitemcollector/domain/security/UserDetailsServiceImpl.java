61eb9c6950fbeab0064e02b4fc38718b6142cb1a
yoodab <yoondab2166@gmail.com>
2024-07-23 04:56:57 +0900

    [#8] ✨ feat : 인증 구현

--- src/main/java/com/sparta/hotitemcollector/domain/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/hotitemcollector/domain/security/UserDetailsServiceImpl.java
@@ -1,12 +1,23 @@
 package com.sparta.hotitemcollector.domain.security;
 
+import com.sparta.hotitemcollector.domain.user.User;
+import com.sparta.hotitemcollector.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByLoginId(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new UserDetailsImpl(user);
     }
 }
