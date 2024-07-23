06e14bd4994d8dfa09bd7e4768a16cd1ae437002
WonGi-Kim <kwg970310@gmail.com>
2024-07-23 17:47:52 +0900

    [#12] ✨ feat : gradle에 iamport 추가 및 컨트롤러 서비스 제한

--- src/main/java/com/sparta/hotitemcollector/domain/payment/PaymentController.java
+++ src/main/java/com/sparta/hotitemcollector/domain/payment/PaymentController.java
@@ -1,9 +1,27 @@
 package com.sparta.hotitemcollector.domain.payment;
 
+import com.sparta.hotitemcollector.global.common.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @GetMapping("/api/payments/prepare-payment")
+    public ResponseEntity<CommonResponse<PaymentRequestDto>> getPreparePayment(@RequestParam String temporaryPaymentId) {
+//        PaymentRequestDto paymentRequest = paymentService.getPaymentRequestData(temporaryPaymentId);
+//        return ResponseEntity.ok(new CommonResponse<>("결제 데이터 준비 완료", 200, paymentRequest));
+        return null;
+    }
+
+    @PostMapping("/api/payments/verify")
+    public ResponseEntity<CommonResponse<String>> verifyPayment(@RequestBody PaymentVerificationDto verificationDto) {
+//        paymentService.verifyPayment(verificationDto);
+//        return ResponseEntity.ok(new CommonResponse<>("결제 검증 완료", 200, "결제 검증 성공"));
+        return null;
+    }
 }
--- src/main/java/com/sparta/hotitemcollector/domain/payment/PaymentService.java
+++ src/main/java/com/sparta/hotitemcollector/domain/payment/PaymentService.java
@@ -1,10 +1,42 @@
 package com.sparta.hotitemcollector.domain.payment;
 
+import com.siot.IamportRestClient.IamportClient;
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentRepository paymentRepository;
+    private IamportClient iamportClient;
+
+    @Value("${REST_API_KEY}")
+    private String apiKey;
+
+    @Value("${REST_SECRET_KEY}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.iamportClient = new IamportClient(apiKey, secretKey);
+    }
+
+//    public String createTemporaryPayment(OrderRequestDto orderRequestDto) {
+//        // 임시 결제 데이터 저장
+//        return temporaryPaymentId;
+//    }
+//
+//    public PaymentRequestDto getPaymentRequestData(String temporaryPaymentId) {
+//        // 임시 결제 데이터 조회
+//        return paymentRequestDto;
+//    }
+//
+//    public void verifyPayment(PaymentVerificationDto verificationDto) {
+//        // 아임포트 API를 호출하여 결제 검증 수행
+//        // 결제 상태 업데이트
+//    }
+
 }
