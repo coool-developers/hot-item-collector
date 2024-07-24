fbc8013efeba52bef6b3c33321722161e50546c1
yoodab <yoondab2166@gmail.com>
2024-07-24 13:27:13 +0900

    [#20] 🩹 fix : Service 이름 변경

--- src/main/java/com/sparta/hotitemcollector/domain/user/controller/AuthController.java
+++ src/main/java/com/sparta/hotitemcollector/domain/user/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.sparta.hotitemcollector.domain.user.controller;
 
 import com.sparta.hotitemcollector.domain.security.UserDetailsImpl;
-import com.sparta.hotitemcollector.domain.user.UserService;
+import com.sparta.hotitemcollector.domain.user.service.AuthService;
 import com.sparta.hotitemcollector.domain.user.dto.LoginReqeustDto;
 import com.sparta.hotitemcollector.domain.user.dto.LoginResponseDto;
 import com.sparta.hotitemcollector.domain.user.dto.RefreshRequestDto;
@@ -18,18 +18,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class AuthController {
 
-    private final UserService userService;
+    private final AuthService authService;
 
     @PostMapping("/signup")
     public ResponseEntity<CommonResponse> signup(@RequestBody SignupRequestDto requestDto) {
-        userService.signup(requestDto);
+        authService.signup(requestDto);
         CommonResponse response = new CommonResponse<>("회원가입 성공",201,"");
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
     public ResponseEntity<CommonResponse> login(@RequestBody LoginReqeustDto requestDto) {
-        LoginResponseDto responseDto = userService.login(requestDto);
+        LoginResponseDto responseDto = authService.login(requestDto);
         CommonResponse response = new CommonResponse<>("로그인 성공",200,responseDto);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -37,7 +37,7 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<CommonResponse> logout(@RequestHeader("Authorization") String accessToken,
                                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        userService.logout(accessToken,userDetails.getUser());
+        authService.logout(accessToken,userDetails.getUser());
         CommonResponse response = new CommonResponse<>("로그아웃 성공",200,"");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -45,14 +45,14 @@ public class AuthController {
     @PatchMapping("/withdraw")
     public ResponseEntity<CommonResponse> withdraw(@RequestHeader("Authorization") String accessToken,
                                                    @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        userService.withdraw(accessToken,userDetails.getUser());
+        authService.withdraw(accessToken,userDetails.getUser());
         CommonResponse response = new CommonResponse<>("회원탈퇴 성공",204,"");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<CommonResponse> refreshToken(@RequestBody RefreshRequestDto refreshRequestDto) {
-        LoginResponseDto responseDto = userService.refreshToken(refreshRequestDto);
+        LoginResponseDto responseDto = authService.refreshToken(refreshRequestDto);
 
         CommonResponse response = new CommonResponse<>("access 토큰 재발급 성공",204,"");
         return new ResponseEntity<>(response, HttpStatus.OK);
