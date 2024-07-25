97fcaf6882f8293a94693d9aee9189461bb4e87c
hongsy521 <seoyeong0521@naver.com>
2024-07-25 11:28:57 +0900

    ✨ feat : ProfileImage 요청/응답 dto 생성

---
+++ src/main/java/com/sparta/hotitemcollector/domain/user/dto/user/ProfileImageResponseDto.java
@@ -0,0 +1,11 @@
+package com.sparta.hotitemcollector.domain.user.dto.user;
+
+import lombok.Getter;
+
+@Getter
+public class ProfileImageResponseDto {
+    private Long id;
+    private String filename;
+    private String imageUrl;
+
+}
