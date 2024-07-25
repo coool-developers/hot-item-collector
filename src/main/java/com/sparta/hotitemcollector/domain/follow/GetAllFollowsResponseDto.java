fc91bcf96ef3159e5d073981d10b7e94012a9896
sujeongmoon <msjk9000@gmail.com>
2024-07-25 23:12:49 +0900

    [#39] 🩹 fix : @Builder 제거

--- src/main/java/com/sparta/hotitemcollector/domain/follow/GetAllFollowsResponseDto.java
+++ src/main/java/com/sparta/hotitemcollector/domain/follow/GetAllFollowsResponseDto.java
@@ -1,21 +1,21 @@
 package com.sparta.hotitemcollector.domain.follow;
 
 import com.sparta.hotitemcollector.domain.user.dto.user.ProfileImageResponseDto;
+
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class GetAllFollowsResponseDto {
-    private Long userId;
-    private ProfileImageResponseDto profileImage;
-    private String profileInfo;
-    private String userName;
+	private Long userId;
+	private ProfileImageResponseDto profileImage;
+	private String profileInfo;
+	private String userName;
 
-
-    @Builder
-    public GetAllFollowsResponseDto(Long userId, ProfileImageResponseDto profileImage, String profileInfo, String userName) {
-        this.userId = userId;
-        this.profileImage = profileImage;
-        this.profileInfo = profileInfo;
-        this.userName = userName;
-    }
+	public GetAllFollowsResponseDto(Long userId, ProfileImageResponseDto profileImage, String profileInfo, String userName) {
+		this.userId = userId;
+		this.profileImage = profileImage;
+		this.profileInfo = profileInfo;
+		this.userName = userName;
+	}
 }
