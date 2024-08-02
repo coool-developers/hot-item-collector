3cb441e74360fe164f1d03df4b4dabac1fc39166
hongsy521 <seoyeong0521@naver.com>
2024-08-02 21:28:15 +0900

    fix:index.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from '@/App';
 import router from '@/router';
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://43.201.84.28:8080';
 
 const app = createApp(App);
-
+app.config.globalProperties.axios = axios;
 app.use(router);
 
 app.mount('#app');
