8e772743c46543a12e9df8d74e7bd74d5072f699
sujeongmoon <msjk9000@gmail.com>
2024-08-05 14:41:29 +0900

    [#77] 🍻 test : orderItemRepositoryTest 1

---
+++ src/test/java/com/sparta/hotitemcollector/domain/orderitem/OrderItemRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.sparta.hotitemcollector.domain.orderitem;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.sparta.hotitemcollector.TestConfig;
+import com.sparta.hotitemcollector.domain.order.OrderStatus;
+import com.sparta.hotitemcollector.domain.product.entity.Product;
+import com.sparta.hotitemcollector.domain.product.repository.ProductRepository;
+
+@DataJpaTest
+// @EnableJpaRepositories(basePackageClasses = OrderItemRepository.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(TestConfig.class)
+class QueryTest {
+
+	@Autowired
+	OrderItemRepository orderItemRepository;
+	@Autowired
+	ProductRepository productRepository;
+
+	@Test
+	@DisplayName("0. orderItemRepository에서 findByOrderId를 테스트한다")
+	void test1() {
+		//given
+		Long id = 83889L;
+
+		// when
+		long before = System.currentTimeMillis();
+		List<OrderItem> orderItems = orderItemRepository.findByOrderId(id);
+		long after = System.currentTimeMillis();
+
+		// then
+		assertNotNull(orderItems);
+		System.out.println("총 걸린 시간 : " + (after - before) + "ms");
+		System.out.println("====================================");
+	}
+
+
+	@Test
+	@DisplayName("1. findAllByStatusAndCreatedAtBetweenAndProductInOrderByCreatedAtDesc를 테스트한다")
+	void test2() {
+		//given
+		LocalDateTime startDate = LocalDateTime.of(2023, 1, 1, 0, 0);
+		LocalDateTime endDate = LocalDateTime.of(2024, 12, 31, 0, 0);
+		List<Long> ids = new ArrayList<>();
+		for (long i = 30000; i <= 33000; i++) {
+			ids.add(i);
+		}
+		List<Product> productList = new ArrayList<>();
+		productList = productRepository.findAllById(ids);
+
+		// when
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>> 메소드 시작 >>>>>>>>>>>>>>>>>>>>>>>>>");
+		long before = System.currentTimeMillis();
+		List<OrderItem> orderItems = orderItemRepository.findAllByStatusAndCreatedAtBetweenAndProductInOrderByCreatedAtDesc(
+			OrderStatus.SHIPMENT_START,
+			startDate,
+			endDate,
+			productList
+		);
+
+		long after = System.currentTimeMillis();
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>> 메소드 종료 >>>>>>>>>>>>>>>>>>>>>>>>>");
+
+		// then
+		System.out.println("총 걸린 시간 : " + (after - before) + "ms");
+		System.out.println("====================================");
+	}
+}
