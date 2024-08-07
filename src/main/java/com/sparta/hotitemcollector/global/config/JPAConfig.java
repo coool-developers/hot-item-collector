a0e828d54ca49a85efa7af3d144f1f8f55b68f42
sujeongmoon <msjk9000@gmail.com>
2024-08-07 11:54:35 +0900

    [#86] 🎉 add : product QueryDSL 세팅

--- src/main/java/com/sparta/hotitemcollector/global/config/JPAConfig.java
+++ src/main/java/com/sparta/hotitemcollector/global/config/JPAConfig.java
@@ -1,16 +1,20 @@
 package com.sparta.hotitemcollector.global.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JPAConfig {
-    @PersistenceContext
-    private EntityManager em;
+	@PersistenceContext
+	private EntityManager em;
 
-//    @Bean
-//    public JPAQueryFactory jpaQueryFactory() {
-//        return new JPAQueryFactory(em);
-//    }
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(em);
+	}
 }
